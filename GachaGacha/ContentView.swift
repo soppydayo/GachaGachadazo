83d1df6cbc05d480ae6f150893fbf30607a55277
soppydayo <souta060614@gmail.com>
2025-06-29 22:50:38 +0900

    Initial Commit

---
+++ GachaGacha/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GachaGacha
+//
+//  Created by 白川創大 on 2025/06/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GachaGacha/GachaGachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GachaGachaApp.swift
+//  GachaGacha
+//
+//  Created by 白川創大 on 2025/06/29.
+//
+
+import SwiftUI
+
+@main
+struct GachaGachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
