a20dc27c42ded28ae6b8bb4f6aea6cc78e911a83
soppydayo <souta060614@gmail.com>
2025-06-29 23:00:09 +0900

    fin

---
+++ GachaGacha/ResultView.swift
@@ -0,0 +1,18 @@
+//
+//  ResultView.swift
+//  GachaGacha
+//
+//  Created by 白川創大 on 2025/06/29.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResultView()
+}
